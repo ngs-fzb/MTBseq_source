1713aa72fa56f6f88d891dfe983133c9a6d09c50
Robin Koch <koch.robin@gmx.net>
2018-01-03 17:10:10 +0100

    Update TBtools.pm

--- lib/TBtools.pm
+++ lib/TBtools.pm
@@ -1456,7 +1456,7 @@ sub help { # Print a help message.
    print
    '
 
-   MTBseq - Copyright (C) 2017   Thomas A. Kohl, Robin Koch, Christian Utpatel,
+   MTBseq - Copyright (C) 2018   Thomas A. Kohl, Robin Koch, Christian Utpatel,
                                  Maria Rosaria De Filippo, Viola Schleusener,
                                  Daniela M. Cirillo, Stefan Niemann
 
